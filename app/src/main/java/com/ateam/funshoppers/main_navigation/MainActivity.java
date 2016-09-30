e950fb6ea81d421d0c0970b8b1a0c1abb1552570
nicks258 <mehrasumit258@gmail.com>
2016-10-01 00:51:28 +0530

    Beacon error fixed

--- app/src/main/java/com/ateam/funshoppers/main_navigation/MainActivity.java
+++ app/src/main/java/com/ateam/funshoppers/main_navigation/MainActivity.java
@@ -7,7 +7,10 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.ateam.funshoppers.R;
 
@@ -77,4 +80,53 @@ public class MainActivity extends AppCompatActivity {
                 mDrawerToggle.syncState();
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * Event Handling for Individual menu item selected
+     * Identify single menu item by it's id
+     * */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        switch (item.getItemId())
+        {
+            case R.id.menu_bookmark:
+                // Single menu item is selected do something
+                // Ex: launching new activity/screen or show alert message
+                Toast.makeText(MainActivity.this, "Bookmark is Selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.menu_save:
+                Toast.makeText(MainActivity.this, "Save is Selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.menu_search:
+                Toast.makeText(MainActivity.this, "Search is Selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.menu_share:
+                Toast.makeText(MainActivity.this, "Share is Selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.menu_delete:
+                Toast.makeText(MainActivity.this, "Delete is Selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.menu_preferences:
+                Toast.makeText(MainActivity.this, "Preferences is Selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
